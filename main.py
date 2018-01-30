6889ae2766734a0f277cba83a46be6f1d4a45e14
GPadley <george.r.padley@gmail.com>
2018-01-30 12:50:02 +0000

    -a

--- main.py
+++ main.py
@@ -11,21 +11,20 @@ xyz = [temp[0],temp[1],temp[2]]
 i2c.writeto_mem(adr[0],0x02,bytearray([0x01]))
 n = 0
 while(1):
-    time.sleep(0.1)
-    data = i2c.readfrom(adr[0],6)
-    temp = unpack('>hhh', data)
-    xyz = [temp[0],temp[1],temp[2]]
+    time.sleep_ms(10) #break time
+    data = i2c.readfrom(adr[0],6) #reads 6 bytes of data, 2 bytes per direction
+    temp = unpack('>hhh', data) #unpacks data into a tuple
+    xyz = [temp[0],temp[1],temp[2]] #converts to list for manipulation
     if n == 0:
-        mean = xyz
-    diff = [xyz[0]-mean[0],xyz[1]-mean[1],xyz[2]-mean[2]]
-    r = (diff[0]*diff[0]+diff[1]*diff[1]+diff[2]*diff[2])
-    if r <= 20:
+        mean = xyz #allows bias averaging
+    diff = [xyz[0]-mean[0],xyz[1]-mean[1],xyz[2]-mean[2]] #normalises data
+    r = (diff[0]*diff[0]+diff[1]*diff[1]+diff[2]*diff[2]) #sees how far away from the norm the data is
+    if r <= 20: #if larger than 20 then recalculate the mean
         if n == 0:
-            mean = xyz
             n += 1
         else:
             mean = [round((n*mean[0]+xyz[0])/(n+1)),round((n*mean[1]+xyz[1])/(n+1)),round((n*mean[2]+xyz[2])/(n+1))]
             n += 1
-    print(diff)
+    print(str(diff[0])+','+str(diff[1])+','+str(diff[2]))
     # i2c.writeto(adr[0],bytearray([0x03]))
     i2c.writeto_mem(adr[0],0x02,bytearray([0x01]))
