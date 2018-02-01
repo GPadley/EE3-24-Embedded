e492c20e73f40909f62f187e71a0a427d067b30d
GPadley <george.r.padley@gmail.com>
2018-02-01 12:02:07 +0000

    -a

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from machine import Pin, I2C
 from ustruct import unpack
+from ujson import dumps
+import network
+from umqtt.simple import MQTTClient
 import time
 i2c = I2C(scl=Pin(5),sda = Pin(4), freq = 100000)
 i2c.start()
@@ -8,7 +11,20 @@ i2c.writeto(adr[0],bytearray([0x70, 0xA0, 0x01]))
 data = i2c.readfrom(adr[0],6)
 temp = unpack('>hhh', data)
 xyz = [temp[0],temp[1],temp[2]]
+key = ['x','y','z']
 i2c.writeto_mem(adr[0],0x02,bytearray([0x01]))
+
+ap_if = network.WLAN(network.AP_IF)
+ap_if.active(False)
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect('EEERover', 'exhibition')
+
+while(not sta_if.isconnected()):
+    pass
+
+client = MQTTClient('OmarnG','192.168.0.10')
+client.connect()
 n = 0
 while(1):
     time.sleep_ms(10) #break time
@@ -25,6 +41,8 @@ while(1):
         else:
             mean = [round((n*mean[0]+xyz[0])/(n+1)),round((n*mean[1]+xyz[1])/(n+1)),round((n*mean[2]+xyz[2])/(n+1))]
             n += 1
-    print(str(diff[0])+','+str(diff[1])+','+str(diff[2]))
+    out = dumps(dict(zip(key,diff)))
+    client.publish('esys/IoT',bytes(out,'utf-8'))
+    # print(out)
     # i2c.writeto(adr[0],bytearray([0x03]))
     i2c.writeto_mem(adr[0],0x02,bytearray([0x01]))
---
+++ receive_jacob.py
@@ -0,0 +1,62 @@
+import paho.mqtt.client as mqtt
+import json
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from collections import deque
+import threading
+
+x = deque(maxlen=1000)
+y = deque(maxlen=1000)
+z = deque(maxlen=1000)
+n = 0
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+
+
+def animate(data):
+    print(data)
+    x.append(data['x'])
+    y.append(data['y'])
+    z.append(data['z'])
+    ax1.clear()
+    ax1.plot(x)
+    ax1.plot(y)
+    ax1.plot(z)
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe('esys/IoT')
+
+# The callback for when a PUBLISH message is received from the server.
+def  on_message(client, userdata, msg):
+    global n
+
+    data = json.loads(msg.payload)
+    ani = animation.FuncAnimation(fig, animate(data), interval=10)
+    n += 1
+    print(n)
+    if n == 100:
+        n = 0
+        plt.show()
+
+thread1 = threading.Thread(target=NAME)
+thread2 = threading.Thread(target=NAME2)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect('192.168.0.10',keepalive=10)
+#
+#
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
