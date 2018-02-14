b8a260abb6a7dc540711941a150dcf3c1b8f2c33
GPadley <george.r.padley@gmail.com>
2018-02-14 16:45:18 +0000

    -a

--- main.py
+++ main.py
@@ -16,18 +16,51 @@ def reset_var(): #reset function
     a = 0;
     data = {'t':0,'s':0,'m':0,'a':0,'d':0} #resets the values
     state = 0 #case statement
-    starttime = utime.ticks_ms() #resets the time of start
-    return n,data,state,starttime,mag,a
+    start_t = utime.ticks_ms() #resets the time of start
+    pre_t = 0
+    return n, data, state, start_t, pre_t, mag, a
 
 def mean_calc(n,mean,input): #creates the mean averaging tool
     if n == 0:
         n += 1
     else:
-        mean = [round((n*mean[0]+input[0])/(n+1)),round((n*mean[1]+input[1])/(n+1)),round((n*mean[2]+input[2])/(n+1))] #recalculates mean
+        mean = round((n*mean+input)/(n+1)) #recalculates mean
         n += 1
     return n, mean
 
+def wifi_conn():
+    ap_if = network.WLAN(network.AP_IF) #connect to wifi
+    ap_if.active(False)
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    sta_if.connect('EEERover', 'exhibition')
+    print('Awaiting WiFi connection...')
+    a = 0
+    while(not sta_if.isconnected()): #check to see if connected before trying to connect to MQTT
+        if a == 0:
+            b_led = Pin(2, Pin.OUT)
+            a = 1
+        else:
+            b_led = Pin(2, Pin.IN)
+            a = 0
+        utime.sleep_ms(50)
+    print('Connected')
+    b_led = Pin(2, Pin.OUT)
 
+def cli_conn(device_id,broker_ip,sub_topic):
+    client = MQTTClient(device_id,broker_ip)  #device ID and the broker IP
+    client.connect() #connect to MQTT
+    client.set_callback(sub_cb) #function which is called when message is received
+    client.subscribe(sub_topic) #where messages from app are recieved from
+    return client
+
+def pub_message(data):
+    out = dumps(data) #transmits dictionary of speed and time between samples
+    client.publish(pub_topic,bytes(out,'utf-8')) #submits to the receiver
+
+def sensor_read(adr):
+    raw = unpack('>hhh', i2c.readfrom(adr,6)) #unpacks data into a tuple
+    return raw[2]
 
 #constants
 start_msg = b'0xFFFFFFFFFFFF' #message from app to start loop
@@ -49,38 +82,22 @@ i2c.writeto_mem(adr[0],0x00,bytearray([0x18])) #tells the sensor to continuously
 i2c.writeto_mem(adr[0],0x01,bytearray([0x80])) #tells the sensor to continuously read data
 i2c.writeto_mem(adr[0],0x02,bytearray([0x01])) #tells the sensor to continuously read data
 
-def sensor_read(adr):
-    raw = unpack('>hhh', i2c.readfrom(adr,6)) #unpacks data into a tuple
-    sensor = [raw[0],raw[1],raw[2]] #converts to list for manipulation
-    return sensor
 
 sensor = sensor_read(adr[0])
 
+wifi_conn()
+
 i2c.writeto_mem(adr[0],0x02,bytearray([0x01])) #tells the sensor to continuously read data
 
-ap_if = network.WLAN(network.AP_IF) #connect to wifi
-ap_if.active(False)
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('EEERover', 'exhibition')
 
-while(not sta_if.isconnected()): #check to see if connected before trying to connect to MQTT
-    pass
+client = cli_conn(device_id,broker_ip,sub_topic)
 
-client = MQTTClient(device_id,broker_ip)  #device ID and the broker IP
-client.connect() #connect to MQTT
-client.set_callback(sub_cb) #function which is called when message is received
-client.subscribe(sub_topic) #where messages from app are recieved from
-
-def pub_message(data):
-    out = dumps(data) #transmits dictionary of speed and time between samples
-    client.publish(pub_topic,bytes(out,'utf-8')) #submits to the receiver
 
 circ = 1.9478 #circumference of wheel in m
-# client.wait_msg()
 while(1):
+    print('Awaiting start message')
     client.wait_msg()
-    n, data, state, starttime, mag, a = reset_var()
+    n, data, state, start_t, pre_t, mag, a = reset_var()
     if rx_msg == start_msg:
 
         print('Connected and verified')
@@ -92,7 +109,7 @@ while(1):
             if rx_msg == reset_msg:
                 rx_msg = b'0x000000000000' #resets so that the values aren't contantly set to 0
                 print('Reset message recieved')
-                n, data, state, starttime, mag, a = reset_var()
+                n, data, state, start_t, pre_t, mag, a = reset_var()
                 pub_message(data)
 
             elif rx_msg == kill_msg:
@@ -101,39 +118,40 @@ while(1):
 
             #reads 6 bytes of data, 2 bytes per direction
             sensor = sensor_read(adr[0])
-
-            print(sensor)
             if n == 0:
-                mean = sensor #allows bias averaging
-            diff = [sensor[0]-mean[0],sensor[1]-mean[1],sensor[2]-mean[2]] #normalises data
+                mean = sensor
 
-            #calculates magnitude of the sensor values
-            mag_avg = sensor[2] #sees how far away from the norm the data is
-            # a = (a+1)%len(mag)
-            # mag_avg = (mag[0]+mag[1]+mag[2])/3
-            if(utime.ticks_diff(utime.ticks_diff(utime.ticks_ms(),starttime),data['t'])>2000): #if the wheel hasn't turned for 2 seconds set speed to 0
+            norm = sensor-mean
+            if(utime.ticks_diff(utime.ticks_diff(utime.ticks_ms(),start_t),data['t'])>2000): #if the wheel hasn't turned for 2 seconds set speed to 0
                 data['s'] = 0 #reset speed
-                data['t'] = utime.ticks_diff(utime.ticks_ms(),starttime) #restart time
-                data['a'] = 1000*data['d']/(utime.ticks_diff(utime.ticks_ms(),starttime))
+                if pre_t == 0:
+                    pre_t = data['t']
+                data['t'] = utime.ticks_diff(utime.ticks_ms(),start_t) #restart time
+                data['a'] = 1000*data['d']/(utime.ticks_diff(utime.ticks_ms(),start_t))
                 pub_message(data) #transmit
 
-            if mag_avg < thresh and state != 1: #looks for impulse from magnet
+            if norm < thresh and state != 1: #looks for impulse from magnet
                 state = 1 #changes state
+                r_led = Pin(0, Pin.OUT)
 
-            elif mag_avg >= thresh and state == 1:  #looks for magnet to be moved away
+            elif norm >= thresh and state == 1:  #looks for magnet to be moved away
+                r_led = Pin(0, Pin.IN)
                 state = 0 #resets state
-                data['s'] = 1000*circ/(utime.ticks_diff(utime.ticks_diff(utime.ticks_ms(),starttime),data['t'])) #calculates speed in m/s and inputs into circular buffer
-                data['t'] = utime.ticks_diff(utime.ticks_ms(),starttime)  #restart time
+                if pre_t != 0:
+                    data['s'] = 1000*circ/(utime.ticks_diff(utime.ticks_diff(utime.ticks_ms(),start_t),pre_t))
+                    pre_t = 0
+                else:
+                    data['s'] = 1000*circ/(utime.ticks_diff(utime.ticks_diff(utime.ticks_ms(),start_t),data['t'])) #calculates speed in m/s and inputs into circular buffer
+                data['t'] = utime.ticks_diff(utime.ticks_ms(),start_t)  #restart time
                 data['d'] += circ #adds on one rotation of the wheel onto the distance travelled
-                data['a'] = 1000*data['d']/(utime.ticks_diff(utime.ticks_ms(),starttime)) #average speed is distance travelled divided by total time
+                data['a'] = 1000*data['d']/(utime.ticks_diff(utime.ticks_ms(),start_t)) #average speed is distance travelled divided by total time
 
                 if data['s'] > data['m']:
                     data['m'] = data['s']
-
                 pub_message(data)
 
-            elif mag_avg <= 10000: #if larger than 400 then recalculate the mean
-                n, mean = mean_calc(n,mean,sensor)
+            elif abs(norm) <= 20:
+                n,mean = mean_calc(n,mean,sensor)
 
             i2c.writeto_mem(adr[0],0x02,bytearray([0x01])) #tells the sensor to read data
 
